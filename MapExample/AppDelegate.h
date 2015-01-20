e8de3afba73c57a5c10ef19824ea21e5703b30f0
reeder32 <nickreeder32@gmail.com>
2015-01-19 18:20:45 -0800

    Initial Commit

---
+++ MapExample/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  MapExample
+//
+//  Created by Nick Reeder on 1/19/15.
+//  Copyright (c) 2015 Nick Reeder. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
